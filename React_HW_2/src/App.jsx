99307d11c8bf639bf19910232102a92a49f95ab2
Tiuleneva_Elizaveta <zyxcser@gmail.com>
2026-06-28 13:01:29 +0900

    Add React homework 2

---
+++ React_HW_2/src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from './components/Greeting'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Greeting name="Александр" />
+    </div>
+  )
+}
+
+export default App
