36f56723d2fd27e285f46e57bde6bcb6da5e0dc7
Tiuleneva_Elizaveta <zyxcser@gmail.com>
2026-06-28 13:24:57 +0900

    Add hw 2.2

--- React_HW_2/src/App.jsx
+++ React_HW_2/src/App.jsx
@@ -1,12 +1,19 @@
 import Greeting from './components/Greeting'
+import ShoppingList from './components/ShoppingList'
 import './App.css'
 
 function App() {
+  const items = ["Кепка", "Шапка", "Очки"];
 
   return (
-    <div>
-      <Greeting name="Александр" />
-    </div>
+    <>
+      <div>
+        <Greeting name="Александр" />
+      </div>
+      <div>
+        <ShoppingList items={items} />
+      </div>
+    </>
   )
 }
 
